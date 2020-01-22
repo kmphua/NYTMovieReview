2be20d852c283ca1b6f609a46badc3d7250d1906
kmphua <kmphua@gmail.com>
2020-01-23 07:36:18 +0800

    Initial Commit

---
+++ NYTMovieReview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTMovieReview
+//
+//  Created by Kevin Phua on 2020/1/23.
+//  Copyright © 2020 HagarSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
